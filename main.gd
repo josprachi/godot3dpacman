bcd5744132ca3af1cb3296dea84883f3bc8b9cea
Prachi Joshi <josprachi@yahoo.com>
2022-12-13 20:54:55 +0530

    character defined with 2 cameras

---
+++ main.gd
@@ -0,0 +1,25 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _input(event):
+	if Input.is_action_pressed("startgame"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		$Camera.current=false
+		$playerbody/pivote/Camera.current=true
+	if Input.is_action_pressed("leavegame"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		$Camera.current=true
+		$playerbody/pivote/Camera.current=false	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
--- playerbody.gd
+++ playerbody.gd
@@ -3,49 +3,59 @@ extends KinematicBody
 # How fast the player moves in meters per second.
 export var speed = 5
 export var jump_impulse = 20
+onready var camera = $pivot/Camera
+
 # The downward acceleration when in the air, in meters per second squared.
 export var fall_acceleration = 75
 
-var velocity = Vector3.ZERO
+#var velocity = Vector3.ZERO
 
-func _process(delta):
+var gravity = -30
+var max_speed = 8
+var mouse_sensitivity = 0.002  # radians/pixel
+var spin = 0.1
+var velocity = Vector3()
+var mouseDelta= Vector2()
+func _ready():
+	#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	pass
-	
-func _physics_process(delta):
-	
-	# We create a local variable to store the input direction.
+func get_input():
 	var direction = Vector3.ZERO
-
 	# We check for each move input and update the direction accordingly.
 	if Input.is_action_pressed("moveright"):
-		direction.x += 1
+		direction+=Vector3.RIGHT
+		#direction.x += 1
 	if Input.is_action_pressed("moveleft"):
-		direction.x -= 1
-	if Input.is_action_pressed("movedown"):      
-		direction.z += 1
+		direction+=Vector3.LEFT
+		#direction.x -= 1
+	if Input.is_action_pressed("movedown"):
+		direction+=Vector3.BACK      
+		#direction.z += 1
 	if Input.is_action_pressed("moveup"):
-		direction.z -= 1
+		direction+=Vector3.FORWARD
+		#direction.z -= 1
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
+		$pivote.look_at(translation - direction,Vector3.UP)		
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y += jump_impulse
-
-	velocity.x = direction.x * speed
-	velocity.z = direction.z * speed
-	velocity.y -= fall_acceleration * delta
-	velocity = move_and_slide(velocity, Vector3.UP)
-	
-#	for index in range(get_slide_count()):
-#        # We check every collision that occurred this frame.
-#		var collision = get_slide_collision(index)
-#        # If we collide with a monster...
-#		if collision.collider.is_in_group("mob"):
-#			var mob = collision.collider
-#            # ...we check that we are hitting it from above.
-#            if Vector3.UP.dot(collision.normal) > 0.1:
-#                # If so, we squash it and bounce.
-#                mob.squash()
-#                velocity.y = bounce_impulse
-
-
-	#print(direction)
+	return direction	
+		
+func _input(event):	
+	if event is InputEventMouseMotion:
+		mouseDelta = event.relative
+		if event.relative.x > 0:
+			rotate_y(-lerp(0, spin, event.relative.x/10))
+		elif event.relative.x < 0:
+			rotate_y(-lerp(0, spin, event.relative.x/10))
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		#rotate_y(-event.relative.x * mouse_sensitivity)
+		$pivote.rotate_x(-event.relative.y * mouse_sensitivity)
+		$pivote.rotation.x = clamp($pivote.rotation.x, -1.2, 1.2)	
+			
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	var desired_velocity = get_input() * max_speed
+	velocity.x = desired_velocity.x
+	velocity.z = desired_velocity.z
+	velocity = move_and_slide(velocity, Vector3.UP, true)
